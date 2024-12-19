5ec1d933e60ddd0b604781827e24d21470d6e087
andara123-web <milikponsel@gmail.com>
2024-12-19 21:32:52 +0700

    Add files via upload

---
+++ modul 07/script_2_doGet().js
@@ -0,0 +1,13 @@
+function doGet(e) {
+
+    var op = e.parameter.action;
+    
+    if (op == "read")
+    
+    return read();
+    
+    else if(op == "insert")
+    
+    return insert_value(e);
+    
+    }
---
+++ modul 07/script_2_insert_value.js
@@ -0,0 +1,23 @@
+//fungsi untuk menambah data 
+function insert_value(request) {
+
+    var wb = SpreadsheetApp.getActiveSpreadsheet();
+    var ws = wb.getSheetByName('BukuTamu');
+    var kode = request.parameter.kode;
+    var nama = request.parameter.nama;
+    var email = request.parameter.email;
+    var pesan = request.parameter.pesan;
+
+    //add new row with received parameter from client
+    var rowData = ws.appendRow([kode, nama, email, pesan]);
+    var result = "Insert successful";
+
+    result = JSON.stringify({
+        "result": result
+    });
+
+    return ContentService
+        .createTextOutput(request.parameter.callback + "(" + result + ")")
+        .setMimeType(ContentService.MimeType.JAVASCRIPT);
+
+}
